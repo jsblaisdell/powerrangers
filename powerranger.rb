83e84137d433c9940dc84e08f0380683033445e4
Justin Blaisdell <jsblaisdell@gmail.com>
2013-09-10 15:06:13 -0500

    initialized person class and added run, scream, and drink_coffee methods.

--- powerranger.rb
+++ powerranger.rb
@@ -1,4 +1,20 @@
 class Person
+  def initialize(name)
+    @name = name
+    @caffeine_level = 0
+  end
+
+  def run
+    "#{@name} ran away."
+  end
+
+  def scream(string)
+    "#{string.upcase}!!!"
+  end
+
+  def drink_coffee
+    @caffeine_level += 1
+  end
 end
 
 class PowerRanger
@@ -6,3 +22,7 @@ end
 
 class EvilNinja
 end
+
+bob = Person.new("Bob")
+puts bob.scream("help")
+puts bob.run
